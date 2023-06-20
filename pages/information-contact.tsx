5659df1891d86e6e999cdcf5526f1e2718bbe0f1
Tayte Boss <boss.tayte@gmail.com>
2023-06-20 17:55:59 +1000

    [style] Information - Structure & responsive styling

---
+++ pages/information-contact.tsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import { getSiteData } from '../lib/datocms';
+import LayoutWrapper from '../components/common/LayoutWrapper';
+import LayoutGrid from '../components/common/LayoutGrid';
+import RichText from '../components/common/RichText';
+import { SiteDataTypes } from '../shared/types/types';
+
+const PageWrapper = styled.div``;
+
+const ContentWrapper = styled.div`
+	grid-column: 1 / 7;
+
+	@media ${(props) => props.theme.mediaBreakpoints.tabletPortrait} {
+		grid-column: 1 / 9;
+	}
+
+	@media ${(props) => props.theme.mediaBreakpoints.mobile} {
+		grid-column: 1 / -1;
+	}
+`;
+
+type Props = {
+	siteData: SiteDataTypes;
+};
+
+const Page = (props: Props) => {
+	const {
+		siteData
+	} = props;
+
+	console.log('siteData', siteData);
+
+	return (
+		<PageWrapper>
+			<LayoutWrapper>
+				<LayoutGrid>
+					<ContentWrapper>
+						{siteData?.informationContact && (
+							<RichText data={siteData.informationContact} />
+						)}
+					</ContentWrapper>
+				</LayoutGrid>
+			</LayoutWrapper>
+		</PageWrapper>
+	);
+};
+
+export async function getStaticProps() {
+	const siteData = await getSiteData();
+
+	return {
+		props: {
+			siteData
+		},
+	};
+}
+
+export default Page;
